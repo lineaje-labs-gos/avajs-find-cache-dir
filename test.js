8e826b0364acec33e9827c64993b68e736586267
Sindre Sorhus <sindresorhus@gmail.com>
2025-04-08 22:23:58 +0700

    Require Node.js 20

--- test.js
+++ test.js
@@ -1,39 +1,38 @@
 import process from 'node:process';
 import fs from 'node:fs';
 import path from 'node:path';
-import {fileURLToPath} from 'node:url';
 import test from 'ava';
 import {deleteSync} from 'del';
 import {temporaryDirectory} from 'tempy';
 import findCacheDirectory from './index.js';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const {dirname} = import.meta;
 
 test('finds from a list of files', t => {
-	process.chdir(path.join(__dirname, '..'));
-	const files = ['foo/bar', 'baz/quz'].map(file => path.join(__dirname, file));
-	t.is(findCacheDirectory({files, name: 'blah'}), path.join(__dirname, 'node_modules', '.cache', 'blah'));
+	process.chdir(path.join(dirname, '..'));
+	const files = ['foo/bar', 'baz/quz'].map(file => path.join(dirname, file));
+	t.is(findCacheDirectory({files, name: 'blah'}), path.join(dirname, 'node_modules', '.cache', 'blah'));
 });
 
 test('finds from process.cwd', t => {
-	process.chdir(path.join(__dirname));
-	t.is(findCacheDirectory({name: 'foo'}), path.join(__dirname, 'node_modules', '.cache', 'foo'));
+	process.chdir(path.join(dirname));
+	t.is(findCacheDirectory({name: 'foo'}), path.join(dirname, 'node_modules', '.cache', 'foo'));
 });
 
 test('finds from options.cwd', t => {
-	process.chdir(path.join(__dirname, '..'));
-	t.is(findCacheDirectory({cwd: __dirname, name: 'bar'}), path.join(__dirname, 'node_modules', '.cache', 'bar'));
+	process.chdir(path.join(dirname, '..'));
+	t.is(findCacheDirectory({cwd: dirname, name: 'bar'}), path.join(dirname, 'node_modules', '.cache', 'bar'));
 });
 
 test('creates directory', t => {
-	const directory = path.join(__dirname, 'node_modules', '.cache', 'created');
+	const directory = path.join(dirname, 'node_modules', '.cache', 'created');
 	deleteSync(directory);
-	findCacheDirectory({create: true, name: 'created', cwd: __dirname});
+	findCacheDirectory({create: true, name: 'created', cwd: dirname});
 	t.true(fs.existsSync(directory));
 });
 
 test('returns undefined if it can\'t find package.json', t => {
-	process.chdir(path.join(__dirname, '..'));
+	process.chdir(path.join(dirname, '..'));
 	t.is(findCacheDirectory({name: 'foo'}), undefined);
 });
 
@@ -53,5 +52,5 @@ test('supports CACHE_DIR environment variable', t => {
 test('ignores `false` for CACHE_DIR environment variable', t => {
 	process.env.CACHE_DIR = 'false';
 
-	t.not(findCacheDirectory(), path.resolve(__dirname, 'false', 'find-cache-directory'));
+	t.not(findCacheDirectory(), path.resolve(dirname, 'false', 'find-cache-directory'));
 });
